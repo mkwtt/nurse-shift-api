4ea2077a2052470af6e5f9d582a35722b7cd5e15
Mickey <mkwtt107@gmail.com>
2025-05-23 21:16:21 +0700

    ADD: Authentication and JWT

---
+++ controllers/auth.controller.js
@@ -0,0 +1,59 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  if (!["nurse", "head_nurse"].includes(role)) {
+    return res.status(400).json({ message: "Invalid role" });
+  }
+
+  try {
+    const [existing] = await db.query(
+      "SELECT * FROM tb_users WHERE email = ?",
+      [email]
+    );
+    if (existing.length > 0)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+    await db.query(
+      "INSERT INTO tb_users (name, email, password, role) VALUES (?, ?, ?, ?)",
+      [name, email, hashed, role]
+    );
+
+    res.status(201).json({ message: "Registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [users] = await db.query("SELECT * FROM tb_users WHERE email = ?", [
+      email,
+    ]);
+    const user = users[0];
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user.id, role: user.role }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      token,
+      user: { id: user.user_id, name: user.name, role: user.role },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 // Routes
-const userRoutes = require("./routes/users.routes");
-const shiftRoutes = require("./routes/shift.routes");
-const leaveRoutes = require("./routes/leave.routes");
+// const userRoutes = require("./routes/users.routes");
+// const shiftRoutes = require("./routes/shift.routes");
+// const leaveRoutes = require("./routes/leave.routes");
+const authRoutes = require("./routes/auth.routes");
 
-app.use("/api/users", userRoutes);
-app.use("/api/shifts", shiftRoutes);
-app.use("/api/leaves", leaveRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/shifts", shiftRoutes);
+// app.use("/api/leaves", leaveRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
